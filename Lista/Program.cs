f540d2f5f05e2d40a0413fe13e875caa66e509d2
Aframmm <89247875+Aframmm@users.noreply.github.com>
2021-12-10 13:44:19 +0100

    Add files via upload

---
+++ Lista/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace hejpådiguppgiftlistor
+{
+    class Program
+    {
+        static int Minsta(List<int> listan) {
+            int minst = listan[0];
+
+            foreach(int i in listan){
+if(i < minst){
+                minst = i;
+                }
+            }
+
+            return minst;
+        }
+
+        static int Största(List<int> listan) {
+            int störst = listan[0];
+
+            foreach(int i in listan){
+                if(i > störst){
+                    störst = i;
+                }
+            }
+
+            return störst;
+        }
+
+        static void SkrivUt(List<int> listan) {
+            foreach(int i in listan){
+                Console.WriteLine(i);
+            }
+        }
+
+        public static void Main (string[] args) {
+            Console.WriteLine("Lista:");
+            List<int> minLista = new List<int>();
+            minLista.Add(11);
+            minLista.Add(5);
+            minLista.Add(12);
+            minLista.Add(3);
+            minLista.Add(6);
+            minLista.Add(3);
+
+            SkrivUt(minLista);
+
+            Console.WriteLine("Största tal: "+Största(minLista));
+            Console.WriteLine("Minsta tal: "+Minsta(minLista));
+
+        }
+    }
+}
